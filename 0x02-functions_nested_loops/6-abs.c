665f500f1fa575c1a316c35dc0c50d236ed6ec68
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2023-07-18 17:21:21 +0100

    C program that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - computes the absolute value of an integer
+* @n: formal parameter
+* Return: return 0
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
