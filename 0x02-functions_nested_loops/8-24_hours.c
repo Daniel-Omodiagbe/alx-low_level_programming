b5dba811a438ad601a9ff69b778da1b4f127e5a2
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2023-07-19 10:53:20 +0100

    C program that prints every minute of te day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints every minute of the day
+* Return: Nothing
+*/
+
+void jack_bauer(void)
+{
+	int hour;
+	int mint;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (mint = 0; mint <= 59; mint++)
+		{
+			_putchar(hour / 10 + 48);
+			_putchar(hour % 10 + 48);
+			_putchar(':');
+			_putchar(mint / 10 + 48);
+			_putchar(mint % 10 + 48);
+			_putchar(10);
+		}
+	}
+}
