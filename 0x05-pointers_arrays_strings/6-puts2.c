dc35698068c529f6280d7404982a32e4984583fb
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2023-07-25 05:32:30 +0100

    made a correction

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,32 @@
 
 void puts2(char *str)
 {
+	int count, i;
+	char *str2;
+
+	str2 = str;
+	count = 0;
 	while (*str != '\0')
 	{
-		_putchar(*str);
-		str = str + 2;
+		count++;
+		str++;
+	}
+	if (count % 2 == 0)
+	{
+		while (*str2 != '\0')
+		{
+			_putchar(*str2);
+			str2 = str2 + 2;
+			count = count - 2;
+		}
+	}
+	else
+	{
+		for (i = 0; i < (count - 1) / 2; i++)
+		{
+			_putchar(*str2);
+			str2 = str2 + 2;
+		}
 	}
 	_putchar(10);
 }
