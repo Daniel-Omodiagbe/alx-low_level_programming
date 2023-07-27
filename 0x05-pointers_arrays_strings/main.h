e1db741b184e3f21031c008d2ae38c9e55f9439d
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2023-07-27 11:20:55 +0100

    made correction

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,3 +10,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
+int _isdigit(int c);
+int _atoi(char *s);
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,27 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
+	int j, i, len = 0;
 
-	for (j = 0; src[j] != '\0' &&  j < n; j++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[j] = src[j];
+		len++;
 	}
-	dest[j] = '\0';
+	if (n < len)
+	{
+		for (j = 0; src[j] != '\0' &&  j < n; j++)
+		{
+			dest[j] = src[j];
+		}
+		return (dest);
+	}
+	else
+	{
+		for (j = 0; src[j] != '\0' && j < n; j++)
+		{
+			dest[j] = src[j];
+		}
+		dest[j] = '\0';
+	}
 	return (dest);
 }
