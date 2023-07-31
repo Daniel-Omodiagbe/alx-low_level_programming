6b9dccfe422e132f183d89661f23847a7bf2420d
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2023-07-31 22:35:01 +0100

    made correction

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,16 +11,12 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
-	if (!accept)
-	{
-		return (NULL);
-	}
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (accept[i] == s[i])
 		{
-			break;
+			return (s + i - 1);
 		}
 	}
-	return (s + i - 1);
+	return (NULL);
 }
