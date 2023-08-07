b804583ad8538681183f3206850285b1d3f753b9
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2023-08-07 14:42:09 +0100

    made correction

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,16 +21,16 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(sizeof(int) * height);
 	}
-	if (!grid)
+	if (grid)
 	{
-		return (NULL);
-	}
-	for (i = 0; i < width; i++)
-	{
-		for (j = 0; j < height; j++)
+		for (i = 0; i < width; i++)
 		{
-			grid[i][j] = 0;
+			for (j = 0; j < height; j++)
+			{
+				grid[i][j] = 0;
+			}
 		}
+		return (grid);
 	}
-	return (grid);
+	return (NULL);
 }
