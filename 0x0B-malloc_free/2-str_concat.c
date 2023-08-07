414a410aa8fd123582486b410af28657d49bb4a6
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2023-08-07 13:53:39 +0100

    made correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,14 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1 = 0, len2 = 0, sum_len = 0;
 	char *str;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		len1++;
@@ -26,19 +34,11 @@ char *str_concat(char *s1, char *s2)
 	{
 		for (i = 0; i < len1; i++)
 		{
-			if (s1 == NULL)
-			{
-				str = "";
-			}
 			str[i] = s1[i];
 		}
 		j = 0;
 		for (; i < sum_len && j < len2; i++)
 		{
-			if (s2 == NULL)
-			{
-				str = "";
-			}
 			str[i] = s2[j];
 			j++;
 		}
