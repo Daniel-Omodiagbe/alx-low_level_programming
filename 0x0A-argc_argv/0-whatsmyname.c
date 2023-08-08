5a1bdec14132ce7821a05214fc171700aa1c17c5
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2023-08-08 10:29:24 +0100

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - entry point
+* @argc: number of arguments
+* @argv: pointer to array of pointers
+* Return: integer
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, count = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		count++;
+	}
+	printf("%s\n", argv[0]);
+	return (count);
+}
