bdebcb916be640266bd0fa846be308daa938dab5
Daniel-Omodiagbe <danielomodiagbe26@gmail.com>
2023-08-16 12:07:02 +0100

    made correction

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - executes a function
+* @array: array of integers
+* @size: size of the array
+* @action: pointer to function needed
+* Return: Nothing
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
+	}
+}
